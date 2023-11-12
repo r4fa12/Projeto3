594367cd68a05e73eb61d113e4791fb4c7f19b98
r4fa12 <148385021+r4fa12@users.noreply.github.com>
2023-11-12 22:11:30 +0000

    Chamando todas as funcoes de Filtrar Tarefas

--- biblioteca.h
+++ biblioteca.h
@@ -11,9 +11,23 @@ typedef struct{
 void Menu();
 
 void ListarTarefas2();
+
 void CriarTarefa();
+
 void DeletarTarefa();
+
 void ListarTarefas();
+
 int criararquivo();
+
 int lerarquivo();
+
 void AlterarTarefas(Tarefa Main[]);
+
+void FiltrarTarefasPrioridade(Tarefa Main[]);
+
+void FiltrarTarefasEstado(Tarefa Main[]);
+
+void FiltrarTarefasCategoria(Tarefa Main[]);
+
+void FiltrarTarefasPC(Tarefa Main[]);
